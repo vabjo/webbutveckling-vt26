3e080249bd0ba8e31f5679b05dd06bcfc36eef24
vabjo <valabjornsd@gmail.com>
2026-01-25 16:44:28 +0100

    -Bilder till photos.html -Responsivitet med css -Försök till komprimering med vite image optimizer men just nu funkar det inte, error: heif: Invalid input: Unspecified: Bitstream not supported by this decoder (2.0)

--- Laboration-1/vite.config.js
+++ Laboration-1/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from "vite";
 import {resolve} from "path";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     build: {
         rollupOptions: {
             input: { /* Lägger till de html filer som ska användas */
                 main: resolve(__dirname, "index.html"),
-                photos: resolve(__dirname, "pics.html"),
+                photos: resolve(__dirname, "photos.html"), /* bäst att döpa samma namn om inte main, annars strul att hitta */
                 process: resolve(__dirname, "process.html")
             }
         }
-    }
+    },
+    plugins: [
+        ViteImageOptimizer({
+            avif: {
+                quality: 60
+            }
+        })
+    ]
 });
